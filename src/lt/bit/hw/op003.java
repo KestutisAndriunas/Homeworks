dc94e07d723ca26cc26f26aa9a70bf1aeaaa71f5
Kestutis Andriunas <kendriunas@gmail.com>
2019-07-22 16:34:16 +0300

    Initial commit

---
+++ src/lt/bit/hw/op003.java
@@ -0,0 +1,12 @@
+package lt.bit.hw;
+
+public class op003 {
+    public static void main(String[] args) {
+        double plotis = 6.25;
+        double ilgis = 3.42;
+        double plotas = plotis * ilgis;
+        double perimetras = (plotis + ilgis*2);
+        System.out.println("Stačiakampio plotas = " + plotas);
+        System.out.println("Stačiakampio perimetras = " + perimetras);
+    }
+}
