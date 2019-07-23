02e0348341fe0b61f625f692fedf1b0449e9405a
Kestutis Andriunas <kendriunas@gmail.com>
2019-07-23 15:19:41 +0300

    op004 completed

--- src/lt/bit/hw/op004.java
+++ src/lt/bit/hw/op004.java
@@ -1,4 +1,9 @@
 package lt.bit.hw;
 
 public class op004 {
+    public static void main(String[] args) {
+        double numberPi = Math.PI;
+        System.out.format("%.5f%n", numberPi );
+    }
+
 }
