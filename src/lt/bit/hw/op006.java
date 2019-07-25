bde045e49ee6589f192778070e653b4cc298d3ed
Kestutis Andriunas <kendriunas@gmail.com>
2019-07-25 20:58:57 +0300

    op006 enhancement

--- src/lt/bit/hw/op006.java
+++ src/lt/bit/hw/op006.java
@@ -1,12 +1,24 @@
 package lt.bit.hw;
 
 /*Parašykite Farenheito į Celsijaus (ºF į ºC) konversijų skaičiuoklę.
-Skaičiavimo tikslumas turi būti du skaičiai po kalblelio.
- */
+Skaičiavimo tikslumas turi būti du skaičiai po kalblelio. */
+
+import java.util.Scanner;
 
 public class op006 {
     public static void main(String[] args) {
-        
+        converterFahrenheitToCelsius();
+    }
+
+    private static void converterFahrenheitToCelsius() {
+        double fahrenheitDegrees;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Fahrenheit degrees: ");
+        fahrenheitDegrees = input.nextDouble();
+        double CelsiusDegrees = (fahrenheitDegrees - 32) / 1.8;
+        System.out.format("If temperature in ºF = " + fahrenheitDegrees +
+                ", temperature in ºC = %.2f%n", CelsiusDegrees);
+
     }
 
 }
